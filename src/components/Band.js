b48af56ef5fcde0e107edf801402df77de2a858d
tkoar <terrancekoar@gmail.com>
2017-07-16 17:28:27 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+
+  deleteBand(event) {
+    this.props.store.dispatch({type: "DELETE_BAND", id: this.props.band.id })
+  }
+
   render() {
     return(
       <div>
-        band component
+        <li>{this.props.band.text}</li>
+        <button onClick={this.deleteBand.bind(this)}>Delete {this.props.band.text}</button>
       </div>
     );
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,13 @@
-export default function manageBand(state = {
-  bands: []
-}, action) {
+let id = 0
+export default function manageBand(state = {bands: []}, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return { bands: state.bands.concat(action.band) }
+      id++
+      const band = Object.assign({}, action.band, { id: id })
+      return { bands: state.bands.concat(band) }
+      case 'DELETE_BAND':
+        const removalIndex = state.bands.findIndex(el => el.id === action.id)
+        return { bands: [...state.bands.slice(0, removalIndex), ...state.bands.slice(removalIndex + 1)] }
     default:
       return state;
   }
